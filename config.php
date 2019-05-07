b43ec25b3a68c4e00d4121accdc7666a105081d1
Qpesa <46829719+Qpesa@users.noreply.github.com>
2019-05-07 18:16:35 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-        'api' => 'http://104.248.79.64:28674',
+        'api' => 'http://104.248.79.64:28675',
         'blockTargetInterval' => 60,
         'coinUnits' => 100000000
 );
